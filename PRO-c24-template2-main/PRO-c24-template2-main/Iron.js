7e997e184a9f645af2313f5cceab52d4d9ad2a05
mansirj <81675108+mansirj@users.noreply.github.com>
2021-06-08 16:10:00 +0530

    Add files via upload

---
+++ PRO-c24-template2-main/PRO-c24-template2-main/Iron.js
@@ -0,0 +1,27 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':0.9,
+        'friction': 0.8,
+        'restitution':1
+      };
+      this.body = Bodies.rectangle(x, y, 100, 50, options);
+      this.width = 150;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      
+  
+      push();
+      translate(pos.x, pos.y);
+      
+      strokeWeight(3);
+      stroke('white')
+      fill('brown')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
---
+++ PRO-c24-template2-main/PRO-c24-template2-main/sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(500,550,60/2)
+    iron = new Iron(350,500,100,50);
+    stone = new Stone(600,550,100,100);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    iron.display();
+    stone.display();
+ 
+}
